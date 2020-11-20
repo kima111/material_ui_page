24e11ceed8e88671d52a4fadc1c97fed6ec80d48
kima111 <kima111@gmail.com>
2020-11-20 13:27:31 -0800

    update

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function index(props) {
     return (
         <div>
-            <div style={{height: '50px'}}></div>
+            <span style={{height: '50px'}}></span>
             {props.children}
         </div>
     )
