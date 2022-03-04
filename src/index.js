4d8cd352d54e56ddfe38efc0e110ea7ee4ba5bc1
Abe Kim <kima111@gmail.com>
2022-03-04 14:36:39 -0800

    update deleted videos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import HttpsRedirect from 'react-https-redirect';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <HttpsRedirect> */}
       <App />
-    {/* </HttpsRedirect> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
